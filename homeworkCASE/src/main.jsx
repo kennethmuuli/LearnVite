19f38d1d045266b66e76e6f0a2c9d8eb59de7439
Kenneth Muuli <kenneth.muuli@gmail.com>
2023-04-12 18:42:47 +0300

    Componentized app: Form.jsx, FilterButton.jsx, Todo.jsx

--- homeworkCASE/src/main.jsx
+++ homeworkCASE/src/main.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+const DATA = [
+  { id: "todo-0", name: "Eat", completed: true },
+  { id: "todo-1", name: "Sleep", completed: false },
+  { id: "todo-2", name: "Repeat", completed: false }
+];
+
+
 import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <App tasks={DATA} />
   </React.StrictMode>,
 )
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <React.StrictMode>
+//     <App tasks={DATA} />
+//   </React.StrictMode>
+// );
